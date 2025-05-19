143f7fa17ad405589976ce8c1eeecec77f934ace
soultef <soul_tefera@yahoo.com>
2025-05-19 13:45:18 -0500

    feat: create RegistrationController with register endpoint

---
+++ auth-service/src/main/java/solatech/auth_service/controllers/RergistrationController.java
@@ -0,0 +1,25 @@
+package solatech.auth_service.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import solatech.auth_service.services.RegistrationService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class RergistrationController {
+
+    private final RegistrationService registrationService;
+    public RergistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+    @RequestMapping("/register")
+    public ResponseEntity<?> register()
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body("user is created");
+    }
+
+
+
+}
