92ee3cc13072e4c68b2fd27beed333d0bc44d436
soultef <soul_tefera@yahoo.com>
2025-05-20 13:39:30 -0500

    feat: added employee dto to confirm registration

---
+++ auth-service/src/main/java/solatech/auth_service/DTO/EmployeesDTO.java
@@ -0,0 +1,32 @@
+package solatech.auth_service.DTO;
+
+public class EmployeesDTO {
+
+    private String fullName;
+    private String email;
+    private String message;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
