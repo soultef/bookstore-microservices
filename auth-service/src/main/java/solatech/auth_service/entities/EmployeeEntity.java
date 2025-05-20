c312583a78f71af05a72f5e6a24cb0fcfa920f83
soultef <soul_tefera@yahoo.com>
2025-05-20 11:42:35 -0500

    feat: add employee entity along with basic fields and getter and setter to map the entity to the table

--- auth-service/src/main/java/solatech/auth_service/entities/EmployeeEntity.java
+++ auth-service/src/main/java/solatech/auth_service/entities/EmployeeEntity.java
@@ -1,6 +1,7 @@
 package solatech.auth_service.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
 
 @Entity
@@ -10,8 +11,69 @@ public class EmployeeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long employeeId;
+    @NotBlank(message = "First name must not be blank")
     private String firstName;
+
+    @NotBlank(message = "Lastname must not be blank")
     private String lastName;
+
+    @NotBlank(message = "email must not be blank")
+    private String email;
+
+    @NotBlank(message = "Social security must not be blank")
+    private String SSN;
+
+    @NotBlank(message = "Employee role must be defined")
     private String role;
+
+    @NotBlank(message = "Employee department must be assigned")
     private String department;
+
+    public @NotBlank(message = "First name must not be blank") String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NotBlank(message = "First name must not be blank") String firstName) {
+        this.firstName = firstName;
+    }
+
+    public @NotBlank(message = "Lastname must not be blank") String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NotBlank(message = "Lastname must not be blank") String lastName) {
+        this.lastName = lastName;
+    }
+
+    public @NotBlank(message = "email must not be blank") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "email must not be blank") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Social security must not be blank") String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(@NotBlank(message = "Social security must not be blank") String SSN) {
+        this.SSN = SSN;
+    }
+
+    public @NotBlank(message = "Employee role must be defined") String getRole() {
+        return role;
+    }
+
+    public void setRole(@NotBlank(message = "Employee role must be defined") String role) {
+        this.role = role;
+    }
+
+    public @NotBlank(message = "Employee department must be assigned") String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(@NotBlank(message = "Employee department must be assigned") String department) {
+        this.department = department;
+    }
 }
