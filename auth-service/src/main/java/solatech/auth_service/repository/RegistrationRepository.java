c6f632726e2c52058d9a1ad4f878c7176047d8d0
soultef <soul_tefera@yahoo.com>
2025-05-20 11:40:18 -0500

    feat:delete CustomerEntity, RegistrationController, and RegistrationRepository for design inconsistency.

--- auth-service/src/main/java/solatech/auth_service/repository/RegistrationRepository.java
+++
@@ -1,7 +0,0 @@
-package solatech.auth_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import solatech.auth_service.entities.UserEntity;
-
-public interface RegistrationRepository extends JpaRepository<UserEntity, Long> {
-}
