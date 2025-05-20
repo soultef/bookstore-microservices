4afb4be0595975215748a226400677c9031517c3
soultef <soul_tefera@yahoo.com>
2025-05-20 13:07:45 -0500

    feat:add annotation validation to the ssn entity field to inforce valid ssn format.

--- auth-service/src/main/java/solatech/auth_service/entities/EmployeeEntity.java
+++ auth-service/src/main/java/solatech/auth_service/entities/EmployeeEntity.java
@@ -26,6 +26,9 @@ public class EmployeeEntity {
     private String email;
 
     @NotBlank(message = "Social security must not be blank")
+    @Pattern(regexp = "^(?!000|666|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0000)\\d{4}$", message = "Invalid SSN format")
+    private String ssn;
+
     private String SSN;
 
     @NotBlank(message = "Employee role must be defined")
