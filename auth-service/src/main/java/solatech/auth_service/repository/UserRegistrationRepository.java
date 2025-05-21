fc29846ca086fa5f31d60774b1056b8f38a64228
soultef <soul_tefera@yahoo.com>
2025-05-21 12:57:58 -0500

    feat: create UserRegistrationRepository with exitByEmail jpa method to check email uniqueness

--- auth-service/src/main/java/solatech/auth_service/repository/UserRegistrationRepository.java
+++ auth-service/src/main/java/solatech/auth_service/repository/UserRegistrationRepository.java
@@ -1,4 +1,9 @@
 package solatech.auth_service.repository;
 
-public class UserRegistrationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import solatech.auth_service.entities.UserEntity;
+
+public interface UserRegistrationRepository extends JpaRepository<UserEntity, Long> {
+
+    public boolean existsByEmail(String email);
 }
