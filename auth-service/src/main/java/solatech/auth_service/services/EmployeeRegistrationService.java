8f7473b64df8211c6cb8fb7bf95cebee86647035
soultef <soul_tefera@yahoo.com>
2025-05-20 23:15:29 -0500

    feat: add employee registration service with duplicate email validation and response DTO mapping

--- auth-service/src/main/java/solatech/auth_service/services/EmployeeRegistrationService.java
+++ auth-service/src/main/java/solatech/auth_service/services/EmployeeRegistrationService.java
@@ -1,7 +1,37 @@
 package solatech.auth_service.services;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import solatech.auth_service.DTO.EmployeesDTO;
+import solatech.auth_service.entities.EmployeeEntity;
+import solatech.auth_service.repository.EmployeeRegistrationRepository;
 
 @Service
-public class RegistrationService {
+public class EmployeeRegistrationService {
+
+    private final EmployeeRegistrationRepository employeeRepo;
+    private EmployeeEntity employeeEntity;
+
+    public EmployeeRegistrationService(EmployeeRegistrationRepository employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    private EmployeesDTO employeesDTOMapper(String fullName, String email, String message)
+    {
+        return new EmployeesDTO(fullName, email, message);
+    }
+
+    public EmployeesDTO registerEmployee(EmployeeEntity employeeEntity) {
+        String fullName = employeeEntity.getFirstName() + " " + employeeEntity.getLastName();
+        String email = employeeEntity.getEmail();
+        String message;
+        if(employeeRepo.existsByEmail(employeeEntity.getEmail())) {
+            message = "Email already exists!";
+            return employeesDTOMapper(fullName, email, message);
+        }
+        employeeRepo.save(employeeEntity);
+        message = "You have successfully registered!";
+        return employeesDTOMapper(fullName, email, message);
+
+    }
 }
