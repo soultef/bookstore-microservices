51ad59bece88be7691823d3bd39551692a8381e0
soultef <soul_tefera@yahoo.com>
2025-05-21 12:01:41 -0500

    feat: create EmployeeLoginController class to handle employee authentication

---
+++ auth-service/src/main/java/solatech/auth_service/controllers/EmployeeLoginController.java
@@ -0,0 +1,9 @@
+package solatech.auth_service.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeLoginController {
+}
